e4e1484c18d1fcfe2a649a33772419150a6de931
erikaordon <erikaog@lcg.unam.mx>
2024-04-14 21:40:51 -0600

    Añadir librería de parseo

--- template-repo/src/conteo_nucleotidos.py
+++ template-repo/src/conteo_nucleotidos.py
@@ -21,27 +21,21 @@ USAGE
 
         
 '''
+import argparse 
+
+parser = argparse.ArgumentParser(description="Conteo de nucleótidos")
+
+# El usuario ingresa el nombre de un archivo, este es obligatorio
+parser.add_argument("input_file", type = str, help='Nombre del archivo a parsear')
 
 # Abre el archivo y lee la secuencia de ADN
-try: 
-    with open('sequence.txt', 'r') as file:
+try:
+    args = parser.parse_args()
+    with open(args.input_file, 'r') as file:
         sequence = file.read()
+    print(f'Proporcion de nucleotidos presentes en la secuencia: A: {sequence.count('A')}, C: {sequence.count('C')}, T:{sequence.count('T')}, G:{sequence.count('G')}')
 
-# Si el archivo no está nombrado como 'sequence.txt' entonces se imprime el siguiente mensaje. 
-except:
-    print("El archivo ingresado no es el correcto, favor de renombrar")
+# En caso de no encontrar el archivo imprime este mensaje de error. 
+except FileNotFoundError:
+    print(f'El archivo no fue encontrado, inténtelo de nuevo')
 
-# Inicializa el contador para cada nucleótido
-count_A = sequence.count('A')
-count_T = sequence.count('T')
-count_G = sequence.count('G')
-count_C = sequence.count('C')
-
-# Muestra los resultados
-print("Número de A's:", count_A)
-print("Número de T's:", count_T)
-print("Número de G's:", count_G)
-print("Número de C's:", count_C)
-
-#Aqui te dejo una version simple en dode no necesariamente debes guardar los valores :)
-print(f'Proporcion de nucleotidos presentes en la secuencia: A: {sequence.count('A')}, C: {sequence.count('C')}, T:{sequence.count('T')}, G:{sequence.count('G')}')
