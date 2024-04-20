ec1173919feb04f5a251b42e0560db0908aff12d
erikaordon <erikaog@lcg.unam.mx>
2024-04-20 16:12:13 -0600

    Imprime mensaje de error si el archivo está vacío
    Además de imprimir el mensaje de error en caso de que el archivo esté vacío, modifiqué las primeras líneas del encabezado del código, documenté de una mejor manera y corregí un error. El error consta en que si el usuario coloca una letra diferente a un nucleótido ahora sí imprime un mensaje de error.

--- template-repo/src/conteo_nucleotidos.py
+++ template-repo/src/conteo_nucleotidos.py
@@ -2,7 +2,7 @@
 NAME Conteo de nucleótidos 
        
 
-VERSION: primera
+VERSION: 3.12.3
         
 
 AUTHOR Erika Ordoñez
@@ -10,23 +10,30 @@ AUTHOR Erika Ordoñez
 
 DESCRIPTION El programa acepta un archivo con el contenido de nucleótidos representados con letras mayúsculas, se utilizan contadores
 para contar la cantidad de As, Ts, CS y Gs para imprimir las cantidades en pantalla.  
-        
-        
 
-USAGE
+CATEGORY DNA sequence  
 
-    % python conteo_nucleotidos.py
+USAGE 
+
+Python conteo_nucleotidos.py
+
+ARGUMENTS
+    posicional [archivo]
+    opcional -n --nucleotido
     
-
-
+SEE ALGO 
+    None
         
 '''
+# Importamos librerías para el parseo y para manejar archivo desde sistema operativo, respectivamente 
 import argparse
 import os
 
 # Crear el parser de argumentos
 parser = argparse.ArgumentParser(description='Contar la frecuencia de nucleótidos en un archivo de secuencia de ADN')
+# El archivo es un argumento obligatorio
 parser.add_argument('archivo', type=str, help='Archivo de secuencia de ADN')
+# Aclarar secuencia, es un argumento opcional, acepta una letra o más. 
 parser.add_argument('-n', '--nucleotido', nargs='+', type=str, help='Nucleótido(s) a contar')
 args = parser.parse_args()
 
@@ -41,15 +48,20 @@ except FileNotFoundError:
         print("sorry, couldn't find the file")
     exit()
 
-# Contar la frecuencia de los nucleótidos
-if args.nucleotido:
-    for nucleotide in args.nucleotido:
-        try:
+# Buscamos el tamaño del archivo, si es diferente a cero se ejecuta el código. 
+if os.path.getsize(args.archivo) != 0:
+    # Contar la frecuencia de los nucleótidos
+    if args.nucleotido:
+        for nucleotide in args.nucleotido:
             count = sequence.count(nucleotide.upper())
-            print(f"Frecuencia de '{nucleotide.upper()}': {count}")
-        except ValueError:
-            print(f"Error: Nucleótido '{nucleotide}' inválido")
-else:
-    for nucleotide in ['A', 'C', 'G', 'T']:
-        count = sequence.count(nucleotide)
-        print(f"Frecuencia de '{nucleotide}': {count}")
+            if count !=0:
+                print(f"Frecuencia de '{nucleotide.upper()}': {count}")
+            # Si la letra no es parte de los nucleótidos, entonces el conteo será cero y se imprimirá un mensaje de error
+            # Esto suponiendo que el archivo del usuario únicamente contiene una secuencia de nucleotidos 
+            else: print(f"Error: Nucleótido '{nucleotide}' es inválido")
+    else:
+        for nucleotide in ['A', 'C', 'G', 'T']:
+            count = sequence.count(nucleotide)
+            print(f"Frecuencia de '{nucleotide}': {count}")
+# si se encuentra que el archivo es tamaño del archivo es cero, entonces significa que está vacío e imprime un mensaje de error. 
+else: print("sorry, the file is empty")
